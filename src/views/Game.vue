a115e85300e527a1243b3855394b66728f04b966
AaronXue <her8153101@yahoo.com.tw>
2020-04-16 15:49:50 +0800

    Add game.vue and link router

---
+++ src/views/Game.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app class="game">
+    <v-container>
+      <h1 class="about-title-right">Project Starry</h1>
+      <h1 class="about-title">No Game, No Life</h1>
+      <h1 class="about-title-opacity">No Game, No Life</h1>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {
+  methods: {
+    changeTitle() {
+      document.title = "Game | Project Starry";
+    }
+  },
+  mounted() {
+    this.changeTitle();
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@900&display=swap");
+.game {
+  font-family: "Noto Sans TC", sans-serif;
+}
+</style>
