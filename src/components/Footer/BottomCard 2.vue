a1d6d0c515b3c937ce84d1666675244e8e0ff1d9
AaronXue <her8153101@yahoo.com.tw>
2020-04-16 16:35:49 +0800

    remove pages that are not used

--- src/components/Footer/BottomCard 2.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-footer dark color="black">
-    <v-card-text class="white--text text-center">
-      <strong>© 2020 Starry Inc. All rights reserved</strong>
-    </v-card-text>
-  </v-footer>
-</template>
-
-<style></style>
