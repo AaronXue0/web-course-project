3a798aeb19ead0f34d3ba1b4347881c84c65bbec
AaronXue0 <her8153101@yahoo.com.tw>
2021-04-13 16:34:19 +0800

    Update settings

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   transpileDependencies: ["vuetify"],
+
+  pluginOptions: {
+    prerenderSpa: {
+      registry: undefined,
+      renderRoutes: ["/", "/About", "Contact"],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true,
+    },
+  },
+
+  publicPath: process.env.NODE_ENV === "production" ? "/project-starry/" : "/",
 };
